4c640cd7206e3dd64f8a0ea45b2fb6f42ef175ff
harvey <alstjr971@naver.com>
2024-12-28 23:56:11 +0900

    docs: 요구사항 추가

---
+++ src/main/java/thymeleaf/spring/domain/item/DeliveryCode.java
@@ -0,0 +1,12 @@
+package thymeleaf.spring.domain.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+}
