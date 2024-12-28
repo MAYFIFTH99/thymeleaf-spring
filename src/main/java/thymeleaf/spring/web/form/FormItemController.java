5769e9d0cb38e1788e5dd1d41894563eec1e6ca8
harvey <alstjr971@naver.com>
2024-12-29 00:58:45 +0900

    feat: radio button, select box
    th:field, th:each 등 반복 사용

--- src/main/java/thymeleaf/spring/web/form/FormItemController.java
+++ src/main/java/thymeleaf/spring/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package thymeleaf.spring.web.form;
 
+import thymeleaf.spring.domain.item.DeliveryCode;
 import thymeleaf.spring.domain.item.Item;
 import thymeleaf.spring.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import thymeleaf.spring.domain.item.ItemType;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,8 +31,22 @@ public class FormItemController {
         return regions;
     }
 
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes(){
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
+    }
     @GetMapping
     public String items(Model model) {
+
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "form/items";
@@ -68,6 +85,9 @@ public class FormItemController {
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
+
+
+
 
 }
 
